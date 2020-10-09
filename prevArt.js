5ae85aec5059245630ecc8db80f8d25cfadfd052
saracop <saracop@users.noreply.github.com>
2020-10-09 15:45:48 +0200

    Update prevArt.js

--- prevArt.js
+++ prevArt.js
@@ -2,7 +2,7 @@ function prevArticle() {
 	var articles = document.getElementsByClassName("issue").children; /*tra tutti gli iframe di tutte le issue: articles è un HTMLcollection */
 	
 	 /*trova quello che viene mostrato, cioè che ha valore "block" alla proprietà display nel css:*/
-	for (var i = 2; i < articles.length; i++) { /* partiamo da i = 2 perché non vogliamo considerare né il primo div figlio (che il div della cover) né il div del primo articolo */
+	for (i = 2; i < articles.length; i++) { /* partiamo da i = 2 perché non vogliamo considerare né il primo div figlio (che il div della cover) né il div del primo articolo */
 		var frame = articles[i];
 		var displayValue = window.getComputedStyle(frame, null).display;
 		if (displayValue === "block") {
