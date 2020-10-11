a81503c64e24c4210322ea86196a53109ebc1ca6
saracop <saracop@users.noreply.github.com>
2020-10-11 12:31:02 +0200

    Update prevArt.js

--- prevArt.js
+++ prevArt.js
@@ -20,10 +20,6 @@ function prevArticle1() {
 		var displayValue = window.getComputedStyle(frame, null).display;
 		if (displayValue === "block") {
 			frame.style.display = "none"; /* ora nascondimelo */
-
-			/*
-			var frameName = frame.class.value; /* voglio il nome del frame, quindi il valore dell'attributo class
-			var frameNum = frameName[6]; estraggo il numero dal nome, che è nascondimelo */
 			articles[i-1].style.display = "block";
 			}
 			
@@ -103,12 +99,12 @@ function prevArticle4() {
 	var articles = document.getElementsByClassName("article");
 	var i;
 
-	for (i = 0; i < articles.lenght; i++) {
-		var frame = articles[i];
-		var displayValue = window.getComputedStyle(frame, null).display;
+	for (i = 1; i < articles.lenght; i++) {
+		var articleDiv = articles[i];
+		var displayValue = window.getComputedStyle(articleDiv, null).display;
 		if (displayValue === "block") {
-			frame1.style.display = "none";
-			articlesIssue1[i-1].style.display = "block";
+			articleDiv.style.display = "none";
+			articles[i-1].style.display = "block";
 			}
 
 	}
