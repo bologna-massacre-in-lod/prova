f90215a296b60ceb96eb7ad99e443ea714173ae7
saracop <saracop@users.noreply.github.com>
2020-10-13 16:08:52 +0200

    Update prevArt.js

--- prevArt.js
+++ prevArt.js
@@ -1,40 +1,31 @@
-// /*
-// scomponi l'id per capire che numero è
-// mostra l'iframe che ha come id il nome - 1 
-// OPPURE
-// data n = posizione dell'articolo mostrato all'interno dei figli dell'issue
-// mostrami il frame che è alla posizione n -1
+/*
+definire una variabile (e.g. articles), cioè un HTMLcollection object che comprenda tutti gli articoli o tutti i children delle issues (i.e. dei div che hanno come classe "issue") che però comprendono anche la copertina
+trova quello che viene mostrato, cioè che ha valore "block" alla proprietà display nel css
+nella ricerca (i.e. for loop) partiamo da i = 1 se articles comprende SOLO gli articoli, perché non vogliamo considerare il primo articolo di ogni issue; oppure da i = 2 se articles contiene TUTTI i figli del div issue, perché non vogliamo considerare il div della cover
+scomponi l'id per capire che numero è
+mostra l'iframe che ha come id il nome - 1 
+OPPURE
+data n = posizione dell'articolo mostrato all'interno dei figli dell'issue
+mostrami il frame che è alla posizione n -1
 
-
-// */
+ */
 
 
 function prevArticle1() {
- 	var articles = document.getElementsByClassName("article"); /* articles è un HTMLCollection che comprende tutti i children dei div che hanno come classe "issue", cioè altri div (4 per ogni issue): uno che è la copertina e i tre articoli */
-
+ 	var articles = document.getElementsByClassName("article");
  	var i;
-
- 	 /*trova quello che viene mostrato, cioè che ha valore "block" alla proprietà display nel css:*/
- 	for (i = 1; i < articles.length; i++) { /* partiamo da i = 2 perché non vogliamo considerare né il primo div figlio (che il div della cover) né il div del primo articolo */
+ 	for (i = 1; i < articles.length; i++) {
  		var frame = articles[i];
  		var displayValue = window.getComputedStyle(frame, null).display;
  		if (displayValue === "block") {
- 			frame.style.display = "none"; /* ora nascondimelo */
+ 			frame.style.display = "none";
  			articles[i-1].style.display = "block";
  		}
-			
-
-
- 			/*
- 			var framePosition = articles.indexOf(frame); non va bene questo metodo per capire a che posizione è il frame displayed, perché vale solo per le stringhe o gli arrays
- 			var prev = articles[framePosition-1];
- 			prev.style.display = "block";
- 			*/
-
-
  	}
 
  }
+
+
 
 
  function prevArticle2() {
@@ -48,7 +39,6 @@ function prevArticle1() {
  			frame1.style.display = "none";
  			articlesIssue1[i-1].style.display = "block";
  		}
-
  	}
 
  	var articlesIssue2 = document.getElementById("issue2").children;
@@ -61,9 +51,7 @@ function prevArticle1() {
  			frame2.style.display = "none";
  			articlesIssue2[c-1].style.display = "block";
  		}
-
  	}
-
  }
 
 
@@ -85,7 +73,6 @@ function prevArticle1() {
  			frame1.style.display = "none";
  			articlesIssue1[i-1].style.display = "block";
  		}
-
  	}
 
  	var articlesIssue2 = document.getElementsByClassName("articleIssue2");
@@ -98,13 +85,16 @@ function prevArticle1() {
  			frame2.style.display = "none";
  			articlesIssue2[c-1].style.display = "block";
  		}
-
  	}
-
  }
 
+
+
+
+
+
  function prevArticle4() {
- 	var articles = document.getElementsByClassName("article"); /* quindi articles = <div class="article1 articleIssue1 article">, <div class="article2 articleIssue1 article">, <div class="article3 articleIssue1 article">, <div class="article1 articleIssue2 article">, <div class="article2 articleIssue2 article">, <div class="article3 articleIssue2 article"> */
+ 	var articles = document.getElementsByClassName("article");
  	var i;
 
  	for (i = 1; i < articles.length; i++) {
@@ -122,7 +112,7 @@ function prevArticle1() {
 
 
 function prevArticle5() {
-	var articles = document.getElementsByClassName("article"); /* quindi articles = <div class="article1 articleIssue1 article">, <div class="article2 articleIssue1 article">, <div class="article3 articleIssue1 article">, <div class="article1 articleIssue2 article">, <div class="article2 articleIssue2 article">, <div class="article3 articleIssue2 article"> */
+	var articles = document.getElementsByClassName("article");
 	var i;
 
 	for (i = 1; i < articles.length; i++) {
@@ -158,10 +148,10 @@ function prevArticle() {
 
 
 function prevArticlee() {
-	var articles = document.getElementsByTagName("iframe"); /* quindi articles = <div class="article1 articleIssue1 article">, <div class="article2 articleIssue1 article">, <div class="article3 articleIssue1 article">, <div class="article1 articleIssue2 article">, <div class="article2 articleIssue2 article">, <div class="article3 articleIssue2 article"> */
+	var articles = document.getElementsByTagName("iframe"); 
 	var i;
 
-	for (i = 1; i < articles.length; i++) {
+	for (i = 2; i < articles.length; i++) {
 		var frame = articles[i],
     		style = window.getComputedStyle(frame),
 			displayValue = style.getPropertyValue('display');
