5c615c9c6cbe2c63929ec36e0df0ea1a3e1a9691
saracop <saracop@users.noreply.github.com>
2020-11-30 11:34:29 +0100

    Update PROVA_js.js

--- PROVA_js.js
+++ PROVA_js.js
@@ -652,8 +652,8 @@ function sortByFreq() {
 		while (numCategories--) { document.getElementById("listIssue" + i).appendChild(document.getElementById("listIssue" + i).childNodes[numCategories]);}
 		for (var n = 0; n < document.getElementById("listIssue" + i).children.length; n++){
 			sortCategory(document.getElementById("listIssue" + i).getElementsByClassName(document.getElementById("listIssue" + i).children[n].className)[0], 'data-frequency');
-			/*var numIstances = document.getElementById("listIssue" + i).children[n].childNodes.length;
-			while (numIstances--) { document.getElementById("listIssue" + i).children[n].appendChild(document.getElementById("listIssue" + i).children[n].childNodes[numIstances]); }*/
+			var numIstances = document.getElementById("listIssue" + i).children[n].children.length;
+			while (numIstances--) { document.getElementById("listIssue" + i).children[n].appendChild(document.getElementById("listIssue" + i).children[n].children[numIstances]); }
 		}
 	}
 
